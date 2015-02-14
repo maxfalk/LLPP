4e6ae7bae6bc4b5d1bfde6f0b62faa402655080c
peralbin <peralbin@ponty.se>
2015-02-14 16:00:08 +0100

    Pthreads in new implementation working.

--- AS2/libpedsim/ped_model.cpp
+++ AS2/libpedsim/ped_model.cpp
@@ -21,8 +21,17 @@ const std::vector<Ped::Crowd*> Ped::Model::getCrowds() const
   return crowds;
 }
 
-void *threaded_tick(void *indices) {
+void *threaded_tick(void *inds) {
     //Pthread here
+    int *indices = (int *) inds;
+    int startIndex = indices[0];
+    int stopIndex = indices[1];
+    for (int i=startIndex; i<stopIndex; i++) {
+        crowd[0]->where_to_go(i);
+        crowd[0]->go(i);
+        crowd[1]->where_to_go(i);
+        crowd[1]->go(i);
+    }
 }
 void Ped::Model::seq()
 {
@@ -36,10 +45,34 @@ void Ped::Model::seq()
    }
 
 }
+void create_threads(Ped::Crowd* c, int nrOfThreads) {
+    pthread_t threads[nrOfThreads];
+    for (int i=0; i<nrOfThreads; i++) {
+        int *indices = (int *) malloc(2*sizeof(int));
+        indices[0] = (c->NumberOfAgents/nrOfThreads)*i;
+        if (nrOfThreads-i == 1) {
+            indices[1] = c->NumberOfAgents/nrOfThreads;
+        } else {
+            indices[1] = (c->NumberOfAgents/nrOfThreads)*(i+1);
+        }
+        pthread_create(&threads[i], NULL, threaded_tick, (void *) indices);
+    }
+}
 void Ped::Model::pThreads()
 {
     //Pthreads here
+    int nrOfThreads1;
+    int nrOfThreads2;
+    if ((nrOfThreads/2)*2 < nrOfThreads) {
+        nrOfThreads1 = nrOfThreads/2;
+        nrOfThreads2 = nrOfThreads/2+1;
+    } else {
+        nrOfThreads1 = nrOfThreads/2;
+        nrOfThreads2 = nrOfThreads/2;
+    }
 
+    create_threads(crowds[0], nrOfThreads1);
+    create_threads(crowds[1], nrOfThreads2);
 
 }
 void Ped::Model::omp()
